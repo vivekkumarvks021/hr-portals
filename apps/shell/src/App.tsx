112216483fccb5368bceb9af0c37c84678703da3
vivekkumarvks021 <vivekkumarvks021@gmail.com>
2026-08-02 16:38:44 +0530

    feat(shell): add routing and integrate employee remote

--- apps/shell/src/App.tsx
+++ apps/shell/src/App.tsx
@@ -1,21 +1,21 @@
-// import AppRoutes from "./routes";
-
-// function App() {
-//   return <AppRoutes />;
-// }
-
-// export default App;
-
-import { lazy, Suspense } from "react";
-
-const EmployeeApp = lazy(() => import("employee/App"));
+import AppRoutes from "./routes";
 
 function App() {
-  return (
-    <Suspense fallback={<h1>Loading...</h1>}>
-      <EmployeeApp />
-    </Suspense>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
+
+// import { lazy, Suspense } from "react";
+
+// const EmployeeApp = lazy(() => import("employee/App"));
+
+// function App() {
+//   return (
+//     <Suspense fallback={<h1>Loading...</h1>}>
+//       <EmployeeApp />
+//     </Suspense>
+//   );
+// }
+
+// export default App;
